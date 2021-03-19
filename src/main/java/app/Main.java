ae899a7d4229fa652c927d02f871e07edfe25332
hayab <h0tnori@vivaldi.net>
2021-03-19 16:08:08 +0900

    Commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,17 +1,44 @@
 package app;
 
+import app.util.GeolocationGetter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import javax.crypto.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class Main extends Application {
 
     protected static Stage page;
     protected static Quiz quiz;
+
+    protected static GeolocationGetter geolocationApi;
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+
+        Cipher cipher=Cipher.getInstance("AES");
+        KeyGenerator keyGen=KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        SecretKey key=keyGen.generateKey();
+        cipher.init(Cipher.ENCRYPT_MODE,key);
+
+        FileInputStream fileInputStream=new FileInputStream(getClass().getResource("/GeolocationGetter.bin").toURI().toString());;
+        CipherInputStream cipherInputStream=new CipherInputStream(fileInputStream,cipher);
+
+        ObjectInputStream objectInputStream=new ObjectInputStream(cipherInputStream);
+        Main.geolocationApi=(GeolocationGetter) objectInputStream.readObject();
+
+        objectInputStream.close();
+        cipherInputStream.close();
+        fileInputStream.close();
+
         Parent root = FXMLLoader.load(getClass().getResource("/Form.fxml"));
         primaryStage.setTitle("LocalQuiz");
         primaryStage.setScene(new Scene(root, 640, 480));
