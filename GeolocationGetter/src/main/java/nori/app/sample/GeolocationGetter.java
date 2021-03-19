1929e5ad4c79cc299f30a331ff36416409440148
hayab <h0tnori@vivaldi.net>
2021-03-19 15:54:48 +0900

    Add GeoLocationProgram Generator.

---
+++ GeolocationGetter/src/main/java/nori/app/sample/GeolocationGetter.java
@@ -0,0 +1,72 @@
+package nori.app.sample;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+public class GeolocationGetter implements Serializable {
+
+    private String appID="";
+
+    public GeolocationGetter(String apiKey){
+        this.appID=apiKey;
+    }
+
+
+    private String[] getLatitudeAndLongitude(HttpClient client) throws IOException, InterruptedException {
+        String publicIPAddress=client.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create("https://ifconfig.io/ip"))
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        ).body();
+
+        //改行コードを取り除く(そうしないとエラー。
+        publicIPAddress=publicIPAddress.substring(0,publicIPAddress.length()-1);
+        String locationMapURI=String.format("https://ipwhois.app/json/%s",publicIPAddress);
+        HttpResponse<String> clientResponse = client.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(locationMapURI))
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+
+
+
+        String body=clientResponse.body();
+        String[] resultLatitudeAndLongitude=new String[2];
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode root=mapper.readTree(body);
+        resultLatitudeAndLongitude[0]=root.get("latitude").toString();
+        resultLatitudeAndLongitude[1]=root.get("longitude").toString();
+        for(int i=0;i<2;i++)
+            resultLatitudeAndLongitude[i]=resultLatitudeAndLongitude[i].replace("\"","");
+        return resultLatitudeAndLongitude;
+    }
+
+    public String getStateJson(HttpClient client,String apiKey) throws IOException, InterruptedException {
+        String[] latudeAndLongitude=this.getLatitudeAndLongitude(client);
+
+        String uriFormat=String.format("https://map.yahooapis.jp/placeinfo/V1/get?appid=%s&lat=%s&lon=%s&output=json",
+                this.appID,
+                latudeAndLongitude[0],
+                latudeAndLongitude[1]);
+        HttpResponse<String> httpResponse=client.send(
+                HttpRequest.newBuilder()
+                        .uri(URI.create(uriFormat))
+                        .GET()
+                        .build(),
+                HttpResponse.BodyHandlers.ofString()
+        );
+        return httpResponse.body();
+    }
+
+}
---
+++ GeolocationGetter/src/main/java/nori/app/sample/Main.java
@@ -0,0 +1,44 @@
+package nori.app.sample;
+
+import javax.crypto.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.http.HttpClient;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GeolocationGetter api = new GeolocationGetter(args[0]);
+        HttpClient client= HttpClient.newBuilder().followRedirects(HttpClient.Redirect.ALWAYS).build();
+
+        try {
+            String stateJson=api.getStateJson(client,args[0]);
+            //暗号化して位置情報取得プログラムをシリアライズする。
+
+            Cipher cipher=Cipher.getInstance("AES");
+            KeyGenerator keyGen=KeyGenerator.getInstance("AES");
+            keyGen.init(128);
+            SecretKey key=keyGen.generateKey();
+            cipher.init(Cipher.ENCRYPT_MODE,key);
+
+            FileOutputStream fileOutputStream=new FileOutputStream("./GeolocationGetter.bin");
+            CipherOutputStream cipherOutputStream=new CipherOutputStream(fileOutputStream,cipher);
+
+            ObjectOutputStream objectOutputStream=new ObjectOutputStream(cipherOutputStream);
+            objectOutputStream.writeObject(api);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+    }
+}
